406baf5fa4bb492271cda2c34365f4514e3875ba
Doğukan Biçer <55893845+dogukan-bicer@users.noreply.github.com>
2021-09-13 16:19:29 +0300

    Update blink.s

--- blink.s
+++ blink.s
@@ -1,6 +1,6 @@
 @ Blinks the LED on pin 21 on Raspberry Pi 4
-@ Orginal version 2017-09-30 Bob Plantz =https://bob.cs.sonoma.edu/IntroCompOrg-RPi/exercises-34.html
 @ Edited for Raspberry Pi 4 2021-09-13 Bicer
+@ Orginal version 2017-09-30 Bob Plantz =https://bob.cs.sonoma.edu/IntroCompOrg-RPi/exercises-34.html
         .cpu    cortex-a72
         .fpu    neon-fp-armv8
         .syntax unified         @ modern syntax
@@ -15,10 +15,9 @@
         .equ    ONE_SEC,1       @ sleep one second
         .equ    PIN21,21        @ pin set bit
         .equ    FILE_DESCRP_ARG,0   @ file descriptor
-        .equ    DEVICE_ARG,4        @ device address
-        .equ    STACK_ARGS,8    @ includes sp 8-byte align
+        .equ    DEVICE_ARG,4    @ device address
 @ The following are defined in /usr/include/asm-generic/fcntl.h:.
-        .equ    O_SYNC,256           @ 04000000|00010000
+        .equ    O_SYNC,256      @ 04000000|00010000
 @ The following are defined in /usr/include/asm-generic/mman-common.h:
         .equ    MAP_SHARED,0x01 @ share changes
 @ Constant program data
@@ -34,7 +33,7 @@ device:
 main:
 @ Open /dev/gpiomem for read/write and syncing        
         ldr     r0, deviceAddr  @ address of /dev/gpiomem
-        ldr     r1, openMode   @ flags for accessing device
+        ldr     r1, openMode    @ flags for accessing device
         bl      open     
         mov     r4, r0          @ use r4 for file descriptor
 @ Map the GPIO registers to a main memory location so we can access them
@@ -48,12 +47,12 @@ main:
         mov     r5, r0          @ use r5 for programming memory address
         mov     r1, PIN21       @ pin to blink
         mov     r2, OUTPUT      @ it's an output
-        mov     r10, r0          @ save pointer to GPIO
-        mov     r11, r1          @ save pin number      
-        udiv    r0, r11, r3      @ GPFSEL number
-        sub     r1, r11, r1      @ for GPFSEL pin
+        mov     r10, r0         @ save pointer to GPIO
+        mov     r11, r1         @ save pin number      
+        udiv    r0, r11, r3     @ GPFSEL number
+        sub     r1, r11, r1     @ for GPFSEL pin
     @ Set up the GPIO pin funtion register in programming memory
-        add     r0, r10, r0      @ GPFSELn address
+        add     r0, r10, r0     @ GPFSELn address
         mov     r3, PIN_FIELD   @ gpio pin field
         mov     r3, PINS_IN_REG @ divisor
 @ All OK, blink the LED 
