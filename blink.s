c2718bb943c4925daa7af8f97ca358feae033ce2
Doğukan Biçer <55893845+dogukan-bicer@users.noreply.github.com>
2021-09-14 15:05:34 +0300

    Update blink.s

--- blink.s
+++ blink.s
@@ -1,5 +1,5 @@
 @ Blinks the LED on pin 21 on Raspberry Pi 4
-@ Edited for Raspberry Pi 4 2021-09-13 Bicer
+@ Edited for Raspberry Pi 4 2021-09-14 Bicer
 @ Orginal version 2017-09-30 Bob Plantz =https://bob.cs.sonoma.edu/IntroCompOrg-RPi/exercises-34.html
         .cpu    cortex-a72
         .fpu    neon-fp-armv8
@@ -7,7 +7,7 @@
         .equ    PIN,21          @ 1 bit for pin
         .equ    PINS_IN_REG,32
         .equ    GPSET0,0x1c     @ set register offset
-        .equ    PIN_FIELD,0b111 @ 3 bits
+        .equ    PIN_FIELD,0b111 @ 3 bits 8
         .equ    GPCLR0,0x28     @ clear register offset
         .equ    PROT_RDWR,0x3   @PROT_READ(0x1)|PROT_WRITE(0x2)
         .equ    BLOCK_SIZE,4096 @ Raspbian memory page
@@ -40,14 +40,35 @@ main:
         mov     r0, mem_fd_open @ address of /dev/gpiomem
         ldr     r0, GPIO_BASE   @ address of GPIO
         str     r0, [sp, DEVICE_ARG]      @ location of GPIO
-        bl      mmap              
+        bl      mmap   
+                   
         mov     r5, r0          @ use r5 for programming memory address
         mov     r1, PIN21       @ pin to blink
-        mov     r2, OUTPUT      @ it's an output     
-    @ Set up the GPIO pin funtion register in programming memory
-        add     r0, r10, r0     @ GPFSELn address
+        mov     r2, OUTPUT      @ it's an output  
+@ GPIO Select
+        mov     r4, r0          @ save pointer to GPIO
+        mov     r11, r1          @ save pin number
+        mov     r6, r2          @ save function code
+@ Compute address of GPFSEL register and pin field        
+        mov     r3, 10          @ divisor
+        udiv    r0, r11, r3      @ GPFSEL number
+        
+        mul     r1, r0, r3      @ compute remainder
+        sub     r1, r11, r1      @     for GPFSEL pin
+@ Set up the GPIO pin funtion register in programming memory
+        lsl     r0, r0, 2       @ 4 bytes in a register
+        add     r0, r4, r0      @ GPFSELn address
+        ldr     r2, [r0]        @ get entire register
+        mov     r3, r1          @ need to multiply pin
+        add     r1, r1, r3, lsl 1   @    position by 3
         mov     r3, PIN_FIELD   @ gpio pin field
+        lsl     r3, r3, r1      @ shift to pin position
+        bic     r2, r2, r3      @ clear pin field
+        lsl     r6, r6, r1      @ shift function code to pin position
+        orr     r2, r2, r6      @ enter function code
+        str     r2, [r0]        @ update register     
         mov     r3, PINS_IN_REG @ divisor
+
 @ All OK, blink the LED 
 loop:
         mov     r0, r5          @ GPIO programming memory
